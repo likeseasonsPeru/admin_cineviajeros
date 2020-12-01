808b6fe3c95398af5e60403174d8a051a369dff6
JAUllauri <ullauri_jeanalex@outlook.com>
2020-12-01 12:08:07 -0500

    [fixed] date format

--- src/utils/jsonToExcel.js
+++ src/utils/jsonToExcel.js
@@ -1,58 +1,47 @@
-import React, { useState,useEffect } from 'react';
-import { exportEmailToExcel } from '../../src/endpoints/index';
-import ExportExcel from 'react-export-excel';
-import moment from 'moment';
+import React, { useState, useEffect } from "react";
+import { exportEmailToExcel } from "../../src/endpoints/index";
+import ExportExcel from "react-export-excel";
+import moment from "moment";
 
 const ExcelFile = ExportExcel.ExcelFile;
 const ExcelSheet = ExportExcel.ExcelSheet;
 const ExcelColumn = ExportExcel.ExcelColumn;
 
-
 export const JsonToExcel = () => {
+  const [emails, setEmails] = useState([]);
 
-    const [emails, setEmails] = useState([]);
-    
-    const getEmailFromBD = async () => {
-        const data = await exportEmailToExcel();
-        if( data) {
-            moment( data.created_at ).calendar();
-            
-            setEmails(data);
-        }
-      }
-
-
-    useEffect(() => {
-        getEmailFromBD();
-        console.log(emails)
-        
-    }, [])
-
-    console.log(emails)
-    try {
-        moment(emails.created_at).calendar()
-    
-    } catch (error) {
-        console.log('No se transformo...')
+  const getEmailFromBD = async () => {
+    const data = await exportEmailToExcel();
+    if (data) {
+      data.forEach(date => {
+        date.created_at =  moment(new Date(date.created_at)).format("DD/MM/YYYY")
+      })
+      setEmails(data);
     }
-    
-    return(
-        <>
-        {
-            emails ? (
-                <ExcelFile element={ <a>Exportar a Excel</a> } filename="Emails">
-                     <ExcelSheet data={ emails } name="Correos suscriptos">
-                        <ExcelColumn label="Emails" value="email" />
-                        <ExcelColumn label="Fecha de creación" value="created_at" />
-                        {/* <ExcelColumn label="created_at" value={ created_at } />  */}
-                    </ExcelSheet> 
-                </ExcelFile>
+  };
 
-            ) : null
-        }
-        
-        </>
-    )
-    
-}
+  useEffect(() => {
+    getEmailFromBD();
+  }, []);
 
+  //console.log(emails);
+  try {
+    moment(emails.created_at).calendar();
+  } catch (error) {
+    console.log("No se transformo...");
+  }
+
+  return (
+    <>
+      {emails ? (
+        <ExcelFile element={<a>Exportar a Excel</a>} filename="Emails">
+          <ExcelSheet data={emails} name="Correos suscriptos">
+            <ExcelColumn label="Emails" value="email" />
+            <ExcelColumn label="Fecha de creación" value="created_at" />
+            {/* <ExcelColumn label="created_at" value={ created_at } />  */}
+          </ExcelSheet>
+        </ExcelFile>
+      ) : null}
+    </>
+  );
+};
